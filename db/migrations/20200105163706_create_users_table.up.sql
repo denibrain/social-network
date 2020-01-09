804e9e3da73f2933d1b737577242aa81808558b6
Denis R <rudev2@mxbud.com>
2020-01-09 11:08:29 +0500

    First commit

---
+++ db/migrations/20200105163706_create_users_table.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE users
+(
+    id int not null AUTO_INCREMENT PRIMARY KEY,
+    name varchar(63) not null,
+    surname varchar(63) not null,
+    age int not null default 0,
+    sex enum('F', 'M', 'U') not null default 'U',
+    `password` varchar(63) not null
+
+)
+    charset=utf8;
+
