e04ac1a017750fab8d2d9832e421cce6d94b72b2
Denis Russkih <denibrain@gmail.com>
2020-01-12 16:29:19 +0500

    Sign up form

--- controllers/auth.go
+++ controllers/auth.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"social-network/model"
+	"strconv"
 	"strings"
 )
 
@@ -14,7 +15,7 @@ const (
 
 func login(c *gin.Context) {
 	session := sessions.Default(c)
-	username := c.PostForm("username")
+	username := c.PostForm("email")
 	password := c.PostForm("password")
 
 	// Validate form input
@@ -38,6 +39,68 @@ func login(c *gin.Context) {
 
 	// Save the username in the session
 	session.Set(userKey, username) // In real world usage you'd set this to the users ID
+	if err := session.Save(); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save session"})
+		return
+	}
+
+	returnTo, ok := c.GetQuery("return_to")
+	if !ok {
+		returnTo = "/"
+	}
+	c.JSON(http.StatusOK, gin.H{"returnTo": returnTo})
+}
+
+func signup(c *gin.Context) {
+	newUser := make(map[string]string)
+	var requiredFields = []string{"email", "password", "name", "surname", "age", "sex", "city"}
+	for _, fieldName := range requiredFields {
+		value := c.PostForm(fieldName)
+		value = strings.Trim(value, " ")
+		if value == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Field can't be empty", "field": fieldName})
+			return
+		}
+		newUser[fieldName] = value
+	}
+
+	age, err := strconv.Atoi(newUser["age"])
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Age should be a number", "field": "age"})
+		return
+	}
+
+	if age < 6 || age > 100 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Age should be in range 6 to 100", "field": "age"})
+		return
+	}
+
+	sex := newUser["sex"]
+	if sex != "U" && sex != "F" && sex != "M" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid value of sex", "field": "sex"})
+		return
+	}
+
+	user := model.User{
+		Login:     newUser["email"],
+		Password:  newUser["password"],
+		Name:      newUser["name"],
+		SurName:   newUser["surname"],
+		Age:       age,
+		Sex:       sex,
+		City:      newUser["city"],
+		Interests: c.PostForm("interests"),
+	}
+
+	err = model.AddUser(user)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+		return
+	}
+
+	session := sessions.Default(c)
+	// Save the username in the session
+	session.Set(userKey, user.Login) // In real world usage you'd set this to the users ID
 	if err := session.Save(); err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save session"})
 		return
---
+++ static/js/signup.js
@@ -0,0 +1,42 @@
+$(".sign-in").submit(function () {
+    $.post("/signup", {
+        email: $("#input-email").val(),
+        password: $("#input-password").val(),
+
+        name: $("#input-name").val(),
+        surname: $("#input-surname").val(),
+
+        city: $("#input-city").val(),
+        interests: $("#input-interests").val(),
+
+        age: $("#input-age").val(),
+        sex: $("#input-sex").val(),
+
+    }, function (response) {
+        document.location = response.returnTo;
+    }, "json")
+    .fail(function(xhr) {
+        if (xhr.status == 400) {
+            if (xhr.responseJSON.field) {
+                let fieldName = $("label[for$=" + xhr.responseJSON.field + "]").text();
+                error(fieldName + ": " +xhr.responseJSON.error);
+            }
+            else
+            {
+                error(xhr.responseJSON.error);
+            }
+            return false;
+        }
+        error("Internal system error")
+    });
+
+    return false;
+});
+
+function error(message) {
+    $(".message-box").html(
+        "<div class=\"alert alert-danger\">" +
+        message +
+        "</div>"
+    )
+}
