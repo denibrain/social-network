a59b0e5c3fc21efe8258d41faa9921503e46dc42
Denis Russkih <denibrain@gmail.com>
2020-01-20 12:08:29 +0500

    Add indexes to users table for name and surname tables

---
+++ db/migrations/20200120120033_alter_users_add_name_index.down.sql
@@ -0,0 +1,2 @@
+DROP INDEX users_name_idx ON users;
+DROP INDEX users_surname_idx ON users;
---
+++ db/migrations/20200120120033_alter_users_add_name_index.up.sql
@@ -0,0 +1,2 @@
+CREATE INDEX users_name_idx ON users (name) USING BTREE;
+CREATE INDEX users_surname_idx ON users (surname) USING BTREE;
